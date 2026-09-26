df01c95f4d93bf5b791c8049ec0d697b3d8baad1
cbfwq3006 <cbfwq3006@163.com>
2026-09-26 17:45:42 +0800

    vault backup: 2026-09-26 17:45:41

---
+++ tmp_specific_utf8.py
@@ -0,0 +1,9 @@
+﻿import json
+obj=json.load(open(r'E:\\sl_obsidian\\tmp_agg_compare.json',encoding='utf8'))
+for n in [41,50,51]:
+ s=obj['summary'][n-1]
+ print('问题',n)
+ print('Word目标:',repr(s['word']['target']))
+ print('Excel目标:',repr(s['excel']['target']))
+ print('Word措施:',repr(s['word']['measure']))
+ print('Excel措施:',repr(s['excel']['measure']))
